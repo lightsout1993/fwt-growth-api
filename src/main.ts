2d2a82fb657ccc2d8743841e4655b653bca710c7
Konstantinov Konstantin <k.konstantinov@framework.team>
2024-04-22 20:35:39 +0400

    feat(common): подменил адаптер на fastify

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+type Adapter = Parameters<typeof NestFactory.create>[1];
+
+const createApp = async <T extends INestApplication>(adapter: Adapter) =>
+  await NestFactory.create<T>(AppModule, adapter);
+
+const bootstrap = async () => {
+  const adapter: Adapter = new FastifyAdapter();
+  const app: INestApplication = await createApp(adapter);
+
   await app.listen(3000);
-}
+};
 
 bootstrap();
