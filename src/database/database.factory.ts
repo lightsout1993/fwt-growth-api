dae1c3a98d75e3c89b7f5b3e5ff1a24a1ffd339e
Konstantinov Konstantin <k.konstantinov@framework.team>
2024-06-10 15:03:17 +0400

    feat(common): исправил конфигурацию для соединения с базой

--- src/database/database.factory.ts
+++ src/database/database.factory.ts
@@ -1,5 +1,13 @@
 import type { ConfigService } from '@nestjs/config';
 
-const databaseFactory = (configService: ConfigService) => configService.get('database');
+const databaseFactory = (configService: ConfigService) => ({
+  host: configService.get('database.host'),
+  port: configService.get('database.port'),
+  type: configService.get('database.type'),
+  database: configService.get('database.database'),
+  entities: configService.get('database.entities'),
+  password: configService.get('database.password'),
+  username: configService.get('database.username'),
+});
 
 export default databaseFactory;
