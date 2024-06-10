cc663f33ec0ddd995843496588cd6f7a748c9ffc
Konstantinov Konstantin <k.konstantinov@framework.team>
2024-06-10 15:36:10 +0400

    fix(common): исправил местоположение сущностей в конфиге базы данных

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,10 +1,10 @@
 const databaseConfig = () => ({
   type: 'postgres' as const,
-  entities: [`${__dirname}/../entities/*.ts`],
   host: process.env.DB_HOST as string,
   database: process.env.DB_NAME as string,
   password: process.env.DB_PASSWORD as string,
   username: process.env.DB_USERNAME as string,
+  entities: [`${__dirname}/../**/*.entity.ts`],
   port: parseInt(process.env.DB_PORT!, 10) || 5432,
 });
 
